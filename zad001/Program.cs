55659bbf671e2b2789044efee54f80eecf194a11
Artem <kypidon71@gmail.com>
2022-09-23 21:44:10 +0300

    addZad001

---
+++ zad001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num2 = num % 100;
+int num3 = num2 / 10;
+Console.WriteLine(num3);
+
+// тут с вводом данного числа, надеюсь правильно понял дз.
