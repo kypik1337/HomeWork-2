48a411f04cddb7a679ad1ba45115b0d25aa0f16b
Artem <kypidon71@gmail.com>
2022-09-23 22:56:28 +0300

    addZad3

---
+++ zad003/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите день недели от 1-7");
+int den = int.Parse(Console.ReadLine());
+if (den == 1)
+    {
+        Console.WriteLine ("NOOOOO");
+    }
+if (den == 2)
+    {
+        Console.WriteLine ("NOOOOO");
+    }
+    if (den == 3)
+    {
+        Console.WriteLine ("NOOOOO");
+    }
+    if (den == 4)
+    {
+        Console.WriteLine ("NOOOOO");
+    }
+    if (den == 5)
+    {
+        Console.WriteLine ("NOOOOO");
+    }
+if (den == 6)
+    {
+        Console.WriteLine ("ММММ Суббота");
+    }
+if (den == 7)
+    {
+        Console.WriteLine ("ММММ Воскресенье");
+    }
