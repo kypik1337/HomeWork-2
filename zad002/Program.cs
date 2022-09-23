640c623f8884065e8ef7e42e2808d10b15a8e470
Artem <kypidon71@gmail.com>
2022-09-23 22:46:03 +0300

    addZad2

---
+++ zad002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numText = Convert.ToString(num);
+if (numText.Length > 2)
+{
+  Console.WriteLine("цифра " + numText[2]);
+}
+else 
+{
+  Console.WriteLine("цифры нет");
+}
